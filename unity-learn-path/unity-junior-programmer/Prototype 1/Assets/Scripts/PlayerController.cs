e29c548616ceffd4ffd23dcecbd0be6edb3926b5
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2021-12-20 18:51:36 +0300

    Add Step into the Driver's Seat

--- unity-learn-path/unity-junior-programmer/Prototype 1/Assets/Scripts/PlayerController.cs
+++ unity-learn-path/unity-junior-programmer/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float verticalInput;
     void Start()
     {
 
@@ -12,6 +15,11 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
