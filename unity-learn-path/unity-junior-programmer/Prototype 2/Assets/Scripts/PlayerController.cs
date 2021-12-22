e5d706f3947b75880c7fb959815d8cc0dc9f36ae
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2021-12-23 01:43:52 +0300

    Add Player Positioning and Random Animal Stampede sections

---
+++ unity-learn-path/unity-junior-programmer/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    private float speed = 30.0f;
+    public float xRange = 20.0f;
+    public GameObject projectilePrefab;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = Vector3.right * -xRange;
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = Vector3.right * xRange;
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
+    }
+}
