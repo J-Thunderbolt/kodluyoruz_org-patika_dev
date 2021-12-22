d13aa90a00bcc6c0f1ed8fcfe51fd6c06107feab
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2021-12-23 02:34:24 +0300

    Add Random Animal Stampede section

---
+++ unity-learn-path/unity-junior-programmer/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20.0f;
+    private float spawnPositionZ = 20.0f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX + 1), 0, spawnPositionZ);
+            int randomAnimalIndex = Random.Range(0, animalPrefabs.Length);
+
+            Instantiate(animalPrefabs[randomAnimalIndex], spawnPosition, animalPrefabs[randomAnimalIndex].transform.rotation);
+        }
+    }
+}
