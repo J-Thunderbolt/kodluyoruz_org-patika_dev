5be7491951358d8b569b6ec69f08bfd82cfdcbfe
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2021-12-28 21:15:29 +0300

    Add Challenge 2 section

---
+++ unity-learn-path/unity-junior-programmer/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float inputDelay = 0.75f;
+    private float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= inputDelay)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timer = 0f;
+            }
+        }
+    }
+}
