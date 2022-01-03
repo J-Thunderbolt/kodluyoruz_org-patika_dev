4c85ed3fda8c83550645edf8641689c679feca9b
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2022-01-03 07:24:51 +0300

    Add Bonus Features 2

---
+++ unity-learn-path/unity-junior-programmer/Prototype 2/Assets/Scripts/AnimalHunger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalHunger : MonoBehaviour
+{
+    public Slider hungerSlider;
+    public int amountToBeFed;
+    private GameManager gameManager;
+    private int currentFedAmount = 0;
+    void Start()
+    {
+        hungerSlider.value = currentFedAmount;
+        hungerSlider.maxValue = amountToBeFed;
+        hungerSlider.fillRect.gameObject.SetActive(false);
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void FeedAnimal(int amount)
+    {
+        currentFedAmount += amount;
+        hungerSlider.fillRect.gameObject.SetActive(true);
+        hungerSlider.value = currentFedAmount;
+
+        if (currentFedAmount >= amountToBeFed)
+        {
+            gameManager.AddScore(amountToBeFed);
+            Destroy(gameObject, 0.1f);
+        }
+    }
+}
--- unity-learn-path/unity-junior-programmer/Prototype 2/Assets/Scripts/PlayerController.cs
+++ unity-learn-path/unity-junior-programmer/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
-    private float speed = 20.0f;
-    public float xRange = 10.0f;
     public GameObject projectilePrefab;
+    private float horizontalInput;
+    private float xRange = 20.0f;
+    private float verticalInput;
+    private float zMin = -1.5f;
+    private float zMax = 15.0f;
+    private float speed = 20.0f;
     void Start()
     {
 
@@ -17,15 +20,26 @@ public class PlayerController : MonoBehaviour
     {
         if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(1, 0, 0) * -xRange;
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(1, 0, 0) * xRange;
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
+        }
+        if (transform.position.z > zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
